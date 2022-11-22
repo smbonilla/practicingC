231928c19ab754ed50db3cd8edacd6af698da270
smbonilla <sierrabonilla14@gmail.com>
2022-11-22 10:59:40 +0000

    showing comments

--- workingWith2D.cpp
+++ workingWith2D.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main(){
 
+    // 2D grid defined by
     int numberGrid[3][2] = {
         {1, 2},
         {3, 4},
         {5, 6}
     };
 
+    /* 
+    
+    long comment example 
+    
+    */
     for(int i = 0; i < 3; i++){
 
         for(int k = 0; k < 2; k++){
