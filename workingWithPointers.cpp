87cd6d49bf1f0c2b4920e5267b9979e66a2a0b47
smbonilla <sierrabonilla14@gmail.com>
2022-11-22 11:11:14 +0000

    pointy pointers

---
+++ workingWithPointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int age = 19; 
+    int *pAge = &age;
+
+
+    double gpa = 2.7;
+    double *pGpa = &gpa;
+
+    string name = "Sierra";
+    string *pName = &name;
+
+    // cout << "Age: " << &age << endl;
+    // cout << "Gpa: " << &gpa << endl;
+    // cout << "Name: " << &name << endl;
+
+    cout << pAge << endl;
+
+    return 0;
+}
