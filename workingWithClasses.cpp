5224a282f922fdc28594eb6e8160009a25132c13
smbonilla <sierrabonilla14@gmail.com>
2022-11-23 12:45:33 +0000

    and constructors

--- workingWithClasses.cpp
+++ workingWithClasses.cpp
@@ -8,17 +8,33 @@ class Book {
         string title;
         string author;
         int pages;
+
+        // constructor function that is called everytime an object
+        // from this class is created
+        Book(string aTitle, string aAuthor, int aPages) {
+            
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+            cout << "Creating Object: " << title << endl;
+
+        }
 };
 
 
 int main(){
 
-    Book book1; 
+    Book book1("Sierra's BIO", "Sierra", 100); 
 
-    book1.title = "Sierra's BIO";
-    book1.author = "Sierra";
-    book1.pages = 100; 
+    Book book2("Nicole's BIO", "Nicole", 100); 
+
+    // book2.title = "Nicole's BIO";
+    // book2.author = "Nicole";
+    // book2.pages = 100; 
 
     cout << book1.title << endl;
+    cout << book2.title << endl;
+
     return 0;
 }
